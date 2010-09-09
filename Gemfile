2c1b0cf64a4c0a538e58e081b379b8972249c9da
Sam Soffes <sam@samsoff.es>
2010-09-09 01:19:03 -0500

    Cleaned up a bit

--- Gemfile
+++ Gemfile
@@ -4,5 +4,4 @@ gem 'sinatra', '1.0'
 
 group :development do
   gem 'shotgun', '0.8'
-  # gem 'mongrel', '1.2.0.pre2'
 end
--- app.rb
+++ app.rb
@@ -1,11 +1,7 @@
 set :app_file, __FILE__
 set :root, File.dirname(__FILE__)
 
-# before do
-#   content_type 'text/html', :charset => 'utf-8'
-# end
-
 get '/?' do
-  # erb :home, :locals => { :params => params}
+  # Just redirect to GitHub for now
   redirect 'http://github.com/samsoffes/sstoolkit'
 end
